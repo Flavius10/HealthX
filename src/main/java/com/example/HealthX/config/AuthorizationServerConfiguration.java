b5fbc12361e69abc439a8ad6c6196f0a4599b992
Flavius10 <flaviusa1010@gmail.com>
2025-10-04 20:48:00 +0300

    Implement an RegisteredClientRepository

--- src/main/java/com/example/HealthX/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/HealthX/config/AuthorizationServerConfiguration.java
@@ -10,12 +10,19 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.util.UUID;
 
 @Configuration
 public class AuthorizationServerConfiguration {
@@ -28,11 +35,14 @@ public class AuthorizationServerConfiguration {
                 new OAuth2AuthorizationServerConfigurer();
 
         http
+                // accepta numai url related cu OAuth2
                 .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
 
+                // activeaza toate configurarile legate de OAuth2 si activeaza OpenID Connect
                 .with(authorizationServerConfigurer ,configurer ->
                         configurer.oidc(Customizer.withDefaults()))
 
+                // in cazul in care da eroare in ceea ce consta authentificarea, redirect catre pagina de login
                 .exceptionHandling(e ->
                         e.authenticationEntryPoint(
                                 new LoginUrlAuthenticationEntryPoint("/login")
@@ -71,6 +81,24 @@ public class AuthorizationServerConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+
+        RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client-id")
+                .clientSecret("secret")
+                .clientAuthenticationMethod(
+                        ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost:8080/login/oauth2/code/my-client")
+                .redirectUri("http://localhost:8080/login/oauth2/code/my-client")
+                .redirectUri("http://localhost:8080/callback")
+                .scope("PROJECT")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(client);
     }
 
 }
