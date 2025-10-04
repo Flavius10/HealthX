2d8334ae635b428e1dd2e98c9e4749ea5e77918b
Flavius10 <flaviusa1010@gmail.com>
2025-10-04 21:12:11 +0300

    Implement a few authorization grant types for client

--- src/main/java/com/example/HealthX/config/OAuth2ClientConfig.java
+++ src/main/java/com/example/HealthX/config/OAuth2ClientConfig.java
@@ -1,4 +1,34 @@
 package com.example.HealthX.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+import java.util.UUID;
+
+@Configuration
 public class OAuth2ClientConfig {
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+
+        RegisteredClient client = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId("client-id")
+                .clientSecret("secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://localhost:8080/oauth2/code/client-id")
+                .scope("PROJECT")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(client);
+    }
+
 }
--- src/main/java/com/example/HealthX/config/UserConfig.java
+++ src/main/java/com/example/HealthX/config/UserConfig.java
@@ -1,4 +1,28 @@
 package com.example.HealthX.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class UserConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        InMemoryUserDetailsManager userMemory = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("Flavius")
+                            .password("password")
+                            .roles("read")
+                            .build();
+
+        userMemory.createUser(user);
+
+        return userMemory;
+    }
+
 }
