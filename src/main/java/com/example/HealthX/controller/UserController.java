c8ba6344a7bf55a462e200c2797289cef69da137
Flavius10 <flaviusa1010@gmail.com>
2025-10-06 22:52:45 +0300

    Map the request to the /users endpoint

--- src/main/java/com/example/HealthX/controller/UserController.java
+++ src/main/java/com/example/HealthX/controller/UserController.java
@@ -2,14 +2,12 @@ package com.example.HealthX.controller;
 
 import com.example.HealthX.domain.User;
 import com.example.HealthX.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
